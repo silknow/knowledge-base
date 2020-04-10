aaa5fd28ffe08688426d605246b634fe8b653b66
Thibault Ehrhart <1208424+ehrhart@users.noreply.github.com>
2020-04-10 12:26:02 +0200

    deploy_all: cd to dir before executing scripts

--- scripts/deploy_all.sh
+++ scripts/deploy_all.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
+CWD=$(pwd)
 SILKNOW_PATH=${SILKNOW_PATH:-"/home/semantic/silknow"}
 CONVERTER_PATH=${CONVERTER_PATH:-"${SILKNOW_PATH}/converter"}
 
 # Load commons
 echo "Loading commons..."
-. ./load_commons.sh
+(cd "${CWD}" && ./load_commons.sh)
 
 # Load museum dumps
 for f in "${CONVERTER_PATH}/output/"*.tar.gz; do
   base=$(basename "${f%.tar.gz}")
   echo "Loading dumps for ${base}..."
-  . ./load_dump.sh "${base}"
+  (cd "${CWD}" && ./load_dump.sh "${base}")
 done
