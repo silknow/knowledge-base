39e33e9a02510511f196578f75ff53dbb9e8131e
Thibault Ehrhart <1208424+ehrhart@users.noreply.github.com>
2020-04-22 13:58:11 +0200

    deploy_all: load prefixes
    Fixes #1

--- scripts/deploy_all.sh
+++ scripts/deploy_all.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
 CWD=$(pwd)
+SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+
 SILKNOW_PATH=${SILKNOW_PATH:-"/home/semantic/silknow"}
 CONVERTER_PATH=${CONVERTER_PATH:-"${SILKNOW_PATH}/converter"}
+CONTAINER_NAME=${CONTAINER_NAME:-"silknow_virtuoso"}
+
+# Load prefixes
+docker cp "${SCRIPTPATH}/prefixes.sql" "${CONTAINER_NAME}:/prefixes.sql"
+docker exec -i "${CONTAINER_NAME}" sh -c "isql-v -U dba -P \${DBA_PASSWORD} < /prefixes.sql"
 
 # Load commons
 echo "Loading commons..."
