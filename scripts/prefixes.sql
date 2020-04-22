cc3b266955c711c4ce31950bff956d96f1b85856
Pasquale Lisena <pasquale.lisena@eurecom.fr>
2020-04-22 11:44:02 +0200

    Create prefixes.sql

---
+++ scripts/prefixes.sql
@@ -0,0 +1,7 @@
+DB.DBA.XML_SET_NS_DECL ('ecrm', 'http://erlangen-crm.org/current/', 2);
+DB.DBA.XML_SET_NS_DECL ('crmdig', 'http://www.ics.forth.gr/isl/CRMext/CRMdig.rdfs/', 2);
+DB.DBA.XML_SET_NS_DECL ('crmsci', 'http://www.ics.forth.gr/isl/CRMsci/', 2);
+DB.DBA.XML_SET_NS_DECL ('time', 'http://www.w3.org/2006/time#', 2);
+DB.DBA.XML_SET_NS_DECL ('schema', 'http://schema.org/', 2);
+DB.DBA.XML_SET_NS_DECL ('geonames', 'http://www.geonames.org/ontology#', 2);
+DB.DBA.XML_SET_NS_DECL ('foaf', 'http://xmlns.com/foaf/0.1/', 2);
