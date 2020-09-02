9120a36e6a48cb5ea1fde354d42a3472dd4ec523
Thibault Ehrhart <1208424+ehrhart@users.noreply.github.com>
2020-09-02 15:58:16 +0200

    deploy_all: fix cd path

--- scripts/deploy_all.sh
+++ scripts/deploy_all.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-CWD=$(pwd)
 SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
 SILKNOW_PATH=${SILKNOW_PATH:-"/home/semantic/silknow"}
@@ -13,11 +12,11 @@ docker exec -i "${CONTAINER_NAME}" sh -c "isql-v -U dba -P \${DBA_PASSWORD} < /p
 
 # Load commons
 echo "Loading commons..."
-(cd "${CWD}" && ./load_commons.sh)
+(cd "${SCRIPTPATH}" && ./load_commons.sh)
 
 # Load museum dumps
 for f in "${CONVERTER_PATH}/output/"*.tar.gz; do
   base=$(basename "${f%.tar.gz}")
   echo "Loading dumps for ${base}..."
-  (cd "${CWD}" && ./load_dump.sh "${base}")
+  (cd "${SCRIPTPATH}" && ./load_dump.sh "${base}")
 done
