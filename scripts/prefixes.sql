5d66e88a1d9bac6ad67773efd9f17eaa7384d004
Thibault Ehrhart <1208424+ehrhart@users.noreply.github.com>
2021-06-30 18:25:32 +0200

    Add silk and prov prefixes

--- scripts/prefixes.sql
+++ scripts/prefixes.sql
@@ -5,3 +5,5 @@ DB.DBA.XML_SET_NS_DECL ('time', 'http://www.w3.org/2006/time#', 2);
 DB.DBA.XML_SET_NS_DECL ('schema', 'http://schema.org/', 2);
 DB.DBA.XML_SET_NS_DECL ('geonames', 'http://www.geonames.org/ontology#', 2);
 DB.DBA.XML_SET_NS_DECL ('foaf', 'http://xmlns.com/foaf/0.1/', 2);
+DB.DBA.XML_SET_NS_DECL ('silk', 'http://data.silknow.org/ontology/', 2);
+DB.DBA.XML_SET_NS_DECL ('prov', 'http://www.w3.org/ns/prov#', 2);
